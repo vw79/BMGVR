7ab6f4d71367a9d96dabf17025f5a16c7739178b
CallMeWho <lewxiuzhen@gmail.com>
2023-09-06 22:37:07 +0800

    Added_06: UI Follow Script
    script added (ui will always follow at player's front view ):
    - can improve to smooth it

---
+++ Assets/Scripts/Assignment/HUD/UI_Follow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script for UI canvas to follow camera (player) view.
+
+public class UI_Follow : MonoBehaviour
+{
+    // for camera
+    [SerializeField] private Transform playerCam_Transform;
+
+    // for UI
+    [SerializeField] private float distanceFromCamera = 1.0f;
+
+    private void LateUpdate()
+    {
+        if (playerCam_Transform != null)
+        {
+            transform.position = playerCam_Transform.position + (playerCam_Transform.forward) * distanceFromCamera;
+            transform.rotation = Quaternion.LookRotation(transform.position - playerCam_Transform.position); //(Need to think its logic)
+        }
+    }
+}
